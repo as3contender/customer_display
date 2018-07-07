025d02a0fd276ee033f1498591f043059fde230e
contender <as3contender@gmail.com>
2018-07-07 13:33:39 +0600

    add delete item function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"strconv"
 	"time"
@@ -27,6 +28,7 @@ type CheckItem struct {
 	Count float64 `json:"count"`
 	Cost  float64 `json:"cost"`
 	Sum   float64 `json:"sum"`
+	Total float64 `json:"total"`
 }
 
 type DisplayString struct {
@@ -45,6 +47,20 @@ type Bus struct {
 	register  chan *websocket.Conn
 	broadcast chan []byte
 	clients   map[*websocket.Conn]bool
+}
+
+func Round(x float64, prec int) float64 {
+	var rounder float64
+	pow := math.Pow(10, float64(prec))
+	intermed := x * pow
+	_, frac := math.Modf(intermed)
+	if frac >= 0.5 {
+		rounder = math.Ceil(intermed)
+	} else {
+		rounder = math.Floor(intermed)
+	}
+
+	return rounder / pow
 }
 
 func (b *Bus) Run() {
@@ -154,22 +170,29 @@ func HandleAddItem(w http.ResponseWriter, r *http.Request) {
 
 	log.Println(params)
 
-	count, err := strconv.ParseFloat(params.Get("count"), 32)
-
+	count, err := strconv.ParseFloat(params.Get("count"), 64)
+	count = Round(count, 2)
 	if err != nil {
 		log.Println(err.Error)
 		return
 	}
 
-	cost, err := strconv.ParseFloat(params.Get("cost"), 32)
-
+	cost, err := strconv.ParseFloat(params.Get("cost"), 64)
+	cost = Round(cost, 2)
 	if err != nil {
 		log.Println(err.Error)
 		return
 	}
 
-	sum, err := strconv.ParseFloat(params.Get("sum"), 32)
+	sum, err := strconv.ParseFloat(params.Get("sum"), 64)
+	sum = Round(sum, 2)
+	if err != nil {
+		log.Println(err.Error)
+		return
+	}
 
+	total, err := strconv.ParseFloat(params.Get("total"), 64)
+	total = Round(total, 2)
 	if err != nil {
 		log.Println(err.Error)
 		return
@@ -179,7 +202,8 @@ func HandleAddItem(w http.ResponseWriter, r *http.Request) {
 		CheckItem{Name: params.Get("name"),
 			Count: count,
 			Cost:  cost,
-			Sum:   sum})
+			Sum:   sum,
+			Total: total})
 	if err != nil {
 		log.Println(err.Error)
 	}
@@ -187,6 +211,71 @@ func HandleAddItem(w http.ResponseWriter, r *http.Request) {
 	body := json.RawMessage(strings)
 
 	var msg Message = Message{Type: "addItem", Body: &body}
+
+	m, err := json.Marshal(msg)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	bus.broadcast <- m
+	w.Write(m)
+
+}
+
+func HandleDelItem(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+
+	params := r.Form
+
+	if len(params) == 0 {
+		w.Write([]byte("No params"))
+		return
+	}
+
+	log.Println(params)
+
+	count, err := strconv.ParseFloat(params.Get("count"), 64)
+	count = Round(count, 2)
+	if err != nil {
+		log.Println(err.Error)
+		return
+	}
+
+	cost, err := strconv.ParseFloat(params.Get("cost"), 64)
+	cost = Round(cost, 2)
+	if err != nil {
+		log.Println(err.Error)
+		return
+	}
+
+	sum, err := strconv.ParseFloat(params.Get("sum"), 64)
+	sum = Round(sum, 2)
+	if err != nil {
+		log.Println(err.Error)
+		return
+	}
+
+	total, err := strconv.ParseFloat(params.Get("total"), 64)
+	total = Round(total, 2)
+	if err != nil {
+		log.Println(err.Error)
+		return
+	}
+
+	strings, err := json.Marshal(
+		CheckItem{Name: params.Get("name"),
+			Count: count,
+			Cost:  cost,
+			Sum:   sum,
+			Total: total})
+	if err != nil {
+		log.Println(err.Error)
+	}
+
+	body := json.RawMessage(strings)
+
+	var msg Message = Message{Type: "delItem", Body: &body}
 
 	m, err := json.Marshal(msg)
 	if err != nil {
@@ -279,6 +368,7 @@ func main() {
 	http.HandleFunc("/addcheck", HandleAddCheck)
 	http.HandleFunc("/addstring", HandleAddString)
 	http.HandleFunc("/additem", HandleAddItem)
+	http.HandleFunc("/delitem", HandleDelItem)
 	http.HandleFunc("/ws", HandleWS)
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))))
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img/"))))
